e6d64decd61ca03105b00d9f78786546bc6f8ae6
Pol Valletbó Montfort <pol.valletbo@glovoapp.com>
2020-10-13 13:02:24 +0200

    Updating package names

--- block.go
+++ block.go
@@ -1,5 +1,5 @@
 // Package plugindemo a demo plugin.
-package block
+package traefik_blocklist
 
 import (
 	"context"
--- checker.go
+++ checker.go
@@ -1,4 +1,4 @@
-package block
+package traefik_blocklist
 
 import (
 	"errors"
